b56c5a543f5af3b66f128bbab2a4e35020a3fea1
ahsankhan1911 <ahsankhan1911@gmail.com>
2018-08-24 10:54:06 +0500

    work for batch file

---
+++ src/NewBatch/components/Preview.jsx
@@ -0,0 +1,211 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
+import { inject, observer } from 'mobx-react';
+import ProgressModal from '../../ProgressModal'
+
+class Preview extends Component {
+
+  constructor(props) {
+    super(props);
+    this.store = this.props.store;
+
+    this.state = {
+      modalMsg : '',
+      progressWidth : 0
+    }
+  }
+
+  componentWillMount() {
+    this.props.store.getExtractedData()
+    // To keep track of ProgressBar
+    this.store.pageCount = 4;
+
+  }
+
+  handleContinue () {
+    var url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+    var promise = new Promise((resolve, reject) => {
+
+      var percentage = 0
+      // Do the usual XHR stuff
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      var intervalId;
+      req.onloadstart = (e) => {
+        document.getElementById('uploading-data').style.display = "block"
+        intervalId = window.setInterval(() => {
+          if (percentage === 99) {
+            clearInterval(intervalId)
+          }
+          percentage++;
+          this.setState({
+            progressWidth: percentage,
+         
+            modalMsg: "Uploading & Importing Data"
+          })
+        }, 90)
+      
+
+      }
+
+      req.onreadystatechange = () => {
+        clearInterval(intervalId)
+      
+        intervalId = window.setInterval(() => {
+          if (percentage === 99) {
+            clearInterval(intervalId)
+          }
+          percentage++;
+          this.setState({
+            progressWidth: percentage
+          })
+        }, 90)
+
+        
+      }
+
+      req.onloadend =  (e) => {
+        if(percentage === 99) {
+          clearInterval(intervalId)
+        }
+        
+        else {
+         
+          this.setState({
+            progressWidth: 100
+          })
+
+          clearInterval(intervalId)
+
+        setTimeout( () => {
+          document.getElementById('progressBar').style.display = "none"
+          document.getElementById('alertInfo').style.display = "block"
+          document.getElementById('modalFooter').style.display = "block"
+
+          this.setState({
+            modalMsg : "Upload Confirmation"
+          })
+
+        }, 1000)
+        }
+       
+
+      }
+      req.onload = function (e) {
+        if (req.status === 200) {
+          // Resolve the promise with the response text
+          resolve(req.response);
+        }
+        else {
+
+          reject(Error(req.statusText));
+        }
+      };
+      req.onabort = () => {
+       
+        clearInterval(intervalId)
+      }
+      req.ontimeout = () => {
+        clearInterval(intervalId)
+      }
+
+      // Handle network errors
+      req.onerror = function () {
+        clearInterval(intervalId)
+      };
+
+      // Make the request
+      req.send();
+    });
+
+    promise.then(response => {
+     
+    })
+  
+  }
+  handleModalBtnClick = () => {
+    this.props.history.push({ pathname: "/main/module-create/wizard/features" })
+  }
+  render() {
+
+    return (
+      <div >
+       <ProgressModal
+        modalMsg={this.state.modalMsg} 
+        progressWidth={this.state.progressWidth}
+        modalAlertMsg= "Data Imported"
+        modalBtnTxt="ok"
+        modalBtnClick = { this.handleModalBtnClick}
+        />
+        <h5 className="wizard-category-subtitle">Preview</h5>
+        <h1 className="wizard-category-title">
+          Select Texts
+            </h1>
+        <h5 className="wizard-subtitle">Select the columns with your texts. Multiple selected columns will be concatenated</h5>
+        <div className="preview">
+          <div className="discard-first-row">
+            <div className="row input-fields">
+              <div className="">
+                <div className="checkbox">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="table-responsive table-preview-flatten">
+            <table className="table">
+              <tbody >
+                <tr>
+                  <td className=""></td>
+                  {
+                    Object.keys(this.props.store.ExtractedData[0]).map((header, index) => {
+                      return (
+
+                        <th id="" key={Math.random()} >
+                          <div className="checkbox">
+                            <label>
+
+                              <span >{header}</span>
+                            </label>
+                          </div>
+                        </th>
+                      )
+                    })}
+                </tr>
+
+                {this.props.store.ExtractedData.map((data, index) => {
+
+                  return (<tr>
+                    <td key={Math.random()} className="td-index">{index + 1}</td>
+                    {
+                      Object.keys(this.props.store.ExtractedData[0]).map((header) => {
+                        return (
+                          <td key={Math.random()} className={header}>{data[header]}</td>
+                        )
+                      })
+                    }
+                  </tr>)
+                })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="text-center margin-top-20">
+          <button type="button"
+            onClick={() => {
+              this.handleContinue()
+            }}
+            className="btn btn-primary continue"
+            disabled="">Continue</button>
+        </div>
+
+       
+      </div>
+    )
+  }
+}
+
+export default withRouter(inject('store')(observer(Preview)));
---
+++ src/NewBatch/index.jsx
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+
+import ChooseFile from './components/ChooseFile'
+import Preview from './components/Preview'
+import { Services } from '../Service'
+import { withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+
+class CreateModel extends Component {
+
+  state ={
+    flag : true
+  }
+  CloseBtnCheck = () => {
+    if (this.props.match.params.childRoute === "name-your-module") {
+      document.getElementById("delete-project").style.display = "block"
+      document.getElementById("delete-project").className = "modal fade in"
+      document.getElementById("overlay").className = "modal-backdrop fade in"
+    }
+    else {
+      this.props.history.push({ pathname: "/main/dashboard" })
+    }
+
+  }
+
+  onClocse =() => {
+    document.getElementById("delete-project").style.display = "none"
+    document.getElementById("delete-project").className = "modal fade"
+    document.getElementById("overlay").className = "fade in"
+  }
+
+  handleStop = () => {
+    this.props.history.push({ pathname: "/main/dashboard" })
+  }
+
+  renderSwitch() {
+    switch (this.props.match.params.childRoute) {
+    
+      case 'choose-file':
+        return <ChooseFile />;
+      case 'preview':
+        return <Preview />;
+   
+      default:
+        return <ChooseFile />
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="current-view-module-create">
+      {!this.state.flag ?  <div className="container container-body">
+          <div className="container-wrapper">
+            <div className="main-content">
+              <div className="tail-spinner" style={{ display: "block" }}></div>
+
+            </div>
+          </div>
+        </div> : <div className="container-body module-create-flatten">
+          <div className="container-wrapper">
+            {/* <div className="progress progress-global">
+              <div className="progress-bar" role="progressbar" style={{ width: Services.increaseProgressBar(8, this.props.store.pageCount) }}>
+                <span className="sr-only"></span>
+              </div>
+            </div> */}
+            <div className="navigation-bar">
+
+              <span onClick={() => {
+                this.props.history.goBack()
+              }} className="typcn typcn-arrow-left clickable"
+             
+              ></span>
+              <span onClick={this.CloseBtnCheck} className="typcn typcn-times clickable"></span>
+            </div>
+            {this.renderSwitch()}
+          </div>
+        </div> }
+       
+        
+
+        {/* Error Dialog for hititng cross button */}
+        <div>
+          <div className="modal fade"  id="delete-project" style={{ display: "none", paddingLeft: "0px" }}>
+            <div className="modal-dialog" style={{ left: "0" }}>
+              <form id="delete-project-form">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <button type="button" onClick={this.onClocse} className="close">×</button>
+                    <h4 className="modal-title" id="myModalLabel" data-bind="text: modal_title">Stop Creation Wizard</h4>
+                  </div>
+
+                  <div className="modal-body">
+                    <div className="alert alert-danger">
+                      <strong>Warning:</strong>
+                      <span >This will delete all your progress with this model.</span>
+                    </div>
+
+                    <p >Are you sure you want to stop the creation wizard?</p>
+                  </div>
+
+                  <div className="modal-footer">
+                    <button type="button" onClick={this.onClocse} className="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="submit" onClick={ this.handleStop} className="btn btn-danger">Stop Creation Wizard</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div id="overlay" class="fade in"></div>
+        </div>
+        {/* Ends */}
+
+      </div>
+
+    )
+  }
+}
+
+export default withRouter(inject('store')(observer(CreateModel)))
